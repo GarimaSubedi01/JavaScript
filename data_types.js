1aa078437bec073022a7e739fa865fba476f9ae3
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-07-12 19:08:17 +0545

    Learned Datatypes

---
+++ data_types.js
@@ -0,0 +1,21 @@
+"use strict"; //treats all JS code as newer version
+//alert (2+2); // undefined- using node.js not browser
+
+let name = "Garima" //string
+let age = 22; //number
+let isLoggeedIn = true //boolean
+let Id; //undefined
+let roll = null; //object
+
+//number => 2^53
+//bigint
+//null => standalone value
+// symbol => used in  react for uniqueness
+
+console.log(typeof 20);
+console.log(typeof null);
+console.log(typeof "Garima");
+console.log(typeof undefined);
+
+
+
