a82c475628e716afec3564e8af62eb8810423992
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-07-13 17:33:13 +0545

    conversion of data  types

---
+++ conversion.js
@@ -0,0 +1,34 @@
+let score = 25;
+let team = "Norway"
+let win = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+console.log(typeof team);
+
+let valueInNumber = Number(team);
+let Win = Number(win);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(Win);
+
+let isLoggeedIn = 1
+let BoolIsLoggedIn = Boolean(isLoggeedIn)
+
+console.log(BoolIsLoggedIn);
+
+let num = 23
+let string = String(num)
+console.log(string);
+console.log(typeof string);
+
+
+
+//Number:
+//"33" => 33
+//"ABC" => Nan
+//true => 1, false => 0
+
+//Boolean:
+// 1 => true; 0 => false
+//"" => false; "anytext" => true;
