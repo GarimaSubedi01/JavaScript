c277a549d9f7d41fbf4653b4becfa615791c97ab
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-07-14 15:38:42 +0545

    learned comparision

---
+++ comparision.js
@@ -0,0 +1,10 @@
+console.log("2">1);
+console.log("02">1);
+console.log(null >= 0);
+console.log(null  == 0);
+console.log(null <= 0);
+console.log("2"===2);
+
+
+//=== => compares data types too
+// different data types comparision are avoided
