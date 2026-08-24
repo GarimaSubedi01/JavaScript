8595189e0fe09c935e85aeef9c11d002f1e8c83e
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-08-24 16:09:01 +0545

    Array part 1

---
+++ Arrar.js
@@ -0,0 +1,29 @@
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["luffy", "sanji"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+/*myArr.push(6)
+myArr.push(7)
+myArr.pop()
+*/
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr)
+
+
+//myArr.unshift(8)
+//console.log(myArr);
+
+//myArr.shift()
+//console.log(myArr.includes(6));
+//console.log(myArr.indexOf(1));
+
+const myn1 = myArr.slice(1,3)
+console.log(myArr);
+console.log(myn1)
+const myn2 = myArr.splice(1,3)
+console.log(myArr);
+console.log(myn2)
+
