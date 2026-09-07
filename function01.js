118a12c85ccd1b1b742eccacf97d00e5d51c168a
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-09-07 14:02:33 +0545

    functions 01

--- function01.js
+++ function01.js
@@ -36,3 +36,29 @@ function loginUserMessage(username="sam"){
 }
 
 console.log(loginUserMessage())
+
+/*function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200, 6000, 7000));*/
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+console.log(calculateCartPrice(200, 6000, 7000));
+const user = {
+    username: "Garima",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject(user)
+
+const myNewArray = [200, 400, 600, 100]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
