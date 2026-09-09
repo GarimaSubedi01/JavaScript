26f0e4e8c3f67fd617c3c7c14807966658aed6d7
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-09-09 21:50:16 +0545

    arrow function

---
+++ Arrow.js
@@ -0,0 +1,43 @@
+const user = {
+    username: "Garima",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+    }
+
+}
+//user.welcomeMessage()
+//user.username = "sam"
+//user.welcomeMessage()
+//console.log(this);
+
+//function chai(){
+//    let username = "garima"
+ //   console.log(this.username);
+//}
+//chai()
+
+//const chai = function(){
+ //   let username = "garima"
+   // console.log(this.username);
+//}
+
+const chai = () =>{
+    let username = "garima"
+    console.log(this);
+}
+
+//chai()
+
+/*const addTwo= (num1, num2)=>{
+    return num1 + num2
+}*/ //explicit return
+
+//const addTwo= (num1, num2)=> num1 + num2
+
+//const addTwo= (num1, num2)=> (num1 + num2) //implicit return
+const addTwo= (num1, num2)=> ({username: "garima"}) //wrap object inside parentheses
+console.log(addTwo(3,4))
