b9195ba676c68caad5cbbd80815aeed4f56cb28c
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-09-09 15:56:29 +0545

    scopes in a function

---
+++ scope.js
@@ -0,0 +1,45 @@
+let a = 200
+if (true){
+    let a = 10
+    const b = 20
+    var c = 30
+    //console.log(a);
+}
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "Garima"
+
+function two(){
+    const website = "youtube"
+    //console.log(username);
+    
+    }
+    //console.log(website);
+    two()   
+}
+one()
+
+if (true){
+    const username = "garima"
+    if(username==="garima"){
+        const website = "youtube"
+        //console.log(username + website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+console.log(addOne(4))
+function addOne(num){
+    return num +1
+
+}
+addTwo(4)
+const addTwo = function(num){
+    return num + 2
+}
