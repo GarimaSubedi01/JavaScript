8e73ed7d132ec4aece017f7e87038716775f1d85
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-09-16 14:39:06 +0545

    control_flow

---
+++ control_flow.js
@@ -0,0 +1,34 @@
+if(2=="2"){
+    console.log("executed");
+    
+}
+if(2==="2"){  //triple for strict checking ie data type
+    console.log("excecuted");
+    
+}
+else{
+    console.log("fahhh");
+    
+}
+
+const score = 100
+
+if(score>50){
+    let power = "fly"
+    console.log(`i believe i can ${power} `);
+     
+}
+ //console.log(`i believe i can ${power} `);
+
+ const age = 67
+
+ if(age<18){
+    console.log("you're a kiddooo");
+ }
+ else if(age<30 && age>19){
+    console.log("you're in your 20s");
+    
+ }
+ else{
+    console.log("hello unc");
+ }
