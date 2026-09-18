3116c0d19e7649cd4afd23312d41010e6f93ce61
GarimaSubedi01 <garimasubedi98@gmail.com>
2026-09-18 23:14:11 +0545

    while and do while loops

---
+++ iterations2.js
@@ -0,0 +1,23 @@
+//while loop 
+let index = 0;
+while(index<=10){
+    console.log(`Value is ${index}`); 
+    index = index + 2;
+}
+
+let arr = 0;
+myArray = ['luffy', 'zoro', 'sanji' ]
+while(arr<myArray.length){
+    console.log(myArray[arr]);
+    arr++;
+    
+}
+
+
+//do while
+let score = 1;
+do{
+    console.log(`Score is ${score}`);
+    score++
+    
+}while(score<=10)
